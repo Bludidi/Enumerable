721a563b707c8b9b31edd0adeb40a7753bfdf199
Lusindiso Ntanjana <lusindisontanjana@gmail.com>
2023-01-06 13:39:48 +0200

    Fix linters

--- class.rb
+++ class.rb
@@ -6,27 +6,27 @@ class MyList
   def initialize(*list)
     @list = list
   end
-end 
+end
 
 # Create a list
 list = MyList.new(1, 2, 3, 4)
 
-# Test #all? method 
+# Test #all? method
 list.all? { |e| e < 5 }
 list.all? { |e| e > 5 }
 
-# Test #any? method 
+# Test #any? method
 list.any? { |e| e == 2 }
 list.any? { |e| e == 5 }
 
-# Test #filter method 
+# Test #filter method
 list.filter(&:even?)
 
-# Test #max method 
+# Test #max method
 list.max
 
-# Test #min method 
-list.min 
+# Test #min method
+list.min
 
-# Test #sort method 
-list.sort 
+# Test #sort method
+list.sort
